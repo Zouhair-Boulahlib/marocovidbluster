97ff79d2c008fd29a781e1bc387fff7c5cbd1f85
the lurking legend <36303840+Zouhair-Boulahlib@users.noreply.github.com>
2020-03-21 19:56:27 +0100

    consistent api for that lib

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const puppeteer = require('puppeteer');
 const URL = 'https://www.marocovid.com/';
+const URL_API = 'https://app.siendogroup.com/api'
 
 
-async function  mssemena(){
-        const browser = await puppeteer.launch({headless: true});
+
+async function  getCovidData(headless = false){
+        headless = !!headless;
+        const browser = await puppeteer.launch({headless});
         const page = await browser.newPage();
         await page.goto(URL);
 
-        const res = await page.waitForResponse('https://app.siendogroup.com/api');
+        const res = await page.waitForResponse(URL_API);
         const data = await res.json();
         await browser.close();
-
        return data;
 
 }
+exports = module.exports;
+module.exports.getCovidData = getCovidData;
